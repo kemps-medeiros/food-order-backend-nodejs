b525e63a6107ea907eb0b88c57a67a52bc6584ae
Kemps Medeiros <kempsmm@gmail.com>
2022-09-07 12:03:21 -0300

    feat: improving createVendor

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,9 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import { ICreateVendorInput } from "../dto";
 import { Vendor } from "../models/Vendor";
+import { GeneratePasswordEncrypted, GenerateSalt } from "../utility";
 
 export const CreateVendor = async (req: Request, res: Response, next: NextFunction) => {
   const { name, address, email, foodType, ownerName, phone, pincode, password } = <ICreateVendorInput>req.body;
+
+  const existingVendor = await Vendor.findOne({ email: email });
+
+  if (existingVendor !== null) {
+    return res.json({ message: "Vendor ALready Exists with this email" });
+  }
+
+  //generate a salt
+  const salt = await GenerateSalt();
+  const passwordEncypted = await GeneratePasswordEncrypted(password, salt);
+
+  //encrypt the password using the salt
 
   const createdVendor = await Vendor.create({
     name,
@@ -13,14 +26,14 @@ export const CreateVendor = async (req: Request, res: Response, next: NextFuncti
     ownerName,
     phone,
     pincode,
-    password,
-    salt: "",
+    password: passwordEncypted,
+    salt: salt,
     rating: 0,
     serviceAvailable: false,
     coverImages: [],
   });
 
-  return res.json({ name, address, email, foodType, ownerName, phone, pincode, password });
+  return res.json(createdVendor);
 };
 
 export const GetVendors = async (req: Request, res: Response, next: NextFunction) => {};
--- models/Vendor.ts
+++ models/Vendor.ts
@@ -38,6 +38,15 @@ const VendorSchema = new Schema(
     // ],
   },
   {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.salt;
+        delete ret.__v;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+      },
+    },
     timestamps: true,
   }
 );
