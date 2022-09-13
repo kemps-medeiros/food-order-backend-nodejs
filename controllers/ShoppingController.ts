700d4fbc9687a5682faffb93b12ec1e7988b62a0
Kemps Medeiros <kempsmm@gmail.com>
2022-09-13 11:16:56 -0300

    feat: creating shopping controller

---
+++ controllers/ShoppingController.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+export const GetFoodAvailability = async (req: Request, res: Response, next: NextFunction) => {};
+
+export const GetTopRestaurants = async (req: Request, res: Response, next: NextFunction) => {};
+
+export const GetFoodsIn30Min = async (req: Request, res: Response, next: NextFunction) => {};
+
+export const SearchFoods = async (req: Request, res: Response, next: NextFunction) => {};
+
+export const RestaurantById = async (req: Request, res: Response, next: NextFunction) => {};
