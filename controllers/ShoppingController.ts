2088c22846e456a28e1c350165fa9ec6c7344d1b
Kemps Medeiros <kempsmm@gmail.com>
2022-09-14 09:30:16 -0300

    feat: implements getFoodsAvailability method

--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -1,6 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import { Vendor } from "../models/Vendor";
 
-export const GetFoodAvailability = async (req: Request, res: Response, next: NextFunction) => {};
+export const GetFoodAvailability = async (req: Request, res: Response, next: NextFunction) => {
+  const pincode = req.params.pincode;
+
+  const result = await Vendor.find({ pincode: pincode, serviceAvailable: true })
+    .sort("rating descending")
+    .populate("foods");
+
+  if (result.length > 0) {
+    return res.status(200).json(result);
+  }
+
+  return res.status(400).json({ message: "Data Not Found" });
+};
 
 export const GetTopRestaurants = async (req: Request, res: Response, next: NextFunction) => {};
 
