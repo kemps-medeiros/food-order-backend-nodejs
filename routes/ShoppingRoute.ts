b3673f528c82b1cf8626b026b3be40fa23b29dbc
Kemps Medeiros <kempsmm@gmail.com>
2022-09-14 09:30:41 -0300

    feat: implements methods on the route

--- routes/ShoppingRoute.ts
+++ routes/ShoppingRoute.ts
@@ -1,20 +1,21 @@
 import express, { Request, Response, NextFunction } from "express";
+import { GetFoodAvailability, GetFoodsIn30Min, GetTopRestaurants, RestaurantById, SearchFoods } from "../controllers";
 
 const router = express.Router();
 
 /** --------------Food Avaiability--------------------- */
-router.get("/:pincode");
+router.get("/:pincode", GetFoodAvailability);
 
 /** --------------Top Restaurants--------------------- */
-router.get("/top-restaurantes/:pincode");
+router.get("/top-restaurantes/:pincode", GetTopRestaurants);
 
 /** --------------Food Available in 30min--------------------- */
-router.get("/foods-in-30-min/:pincode");
+router.get("/foods-in-30-min/:pincode", GetFoodsIn30Min);
 
 /** --------------Search Foods--------------------- */
-router.get("/search/:pincode");
+router.get("/search/:pincode", SearchFoods);
 
 /** --------------Find Restaurant By ID--------------------- */
-router.get("/restaurants/:id");
+router.get("/restaurants/:id", RestaurantById);
 
 export { router as ShoppingRoute };
