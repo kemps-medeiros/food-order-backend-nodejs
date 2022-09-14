944a9a7aec2300ba3edc083ae64bab99261a7a5c
Kemps Medeiros <kempsmm@gmail.com>
2022-09-14 09:55:42 -0300

    feat: implment getFoodIn 30MIn method

--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { FoodDoc } from "../models/Food";
 import { Vendor } from "../models/Vendor";
 
 export const GetFoodAvailability = async (req: Request, res: Response, next: NextFunction) => {
@@ -27,7 +28,25 @@ export const GetTopRestaurants = async (req: Request, res: Response, next: NextF
   return res.status(400).json({ message: "Data Not Found" });
 };
 
-export const GetFoodsIn30Min = async (req: Request, res: Response, next: NextFunction) => {};
+export const GetFoodsIn30Min = async (req: Request, res: Response, next: NextFunction) => {
+  const pincode = req.params.pincode;
+
+  const result = await Vendor.find({ pincode: pincode, serviceAvailable: true }).populate("foods");
+
+  if (result.length > 0) {
+    let foodResult: any = [];
+
+    result.map((vendor) => {
+      const foods = vendor.foods as [FoodDoc];
+
+      foodResult.push(...foods.filter((food) => food.readyTime <= 30));
+    });
+
+    return res.status(200).json(foodResult);
+  }
+
+  return res.status(400).json({ message: "Data Not Found" });
+};
 
 export const SearchFoods = async (req: Request, res: Response, next: NextFunction) => {};
 
