f38eca349269b3a053dbb1ba34d81af908dfac74
Kemps Medeiros <kempsmm@gmail.com>
2022-09-14 09:35:43 -0300

    feat: implements getTopRestaurantes method

--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -15,7 +15,17 @@ export const GetFoodAvailability = async (req: Request, res: Response, next: Nex
   return res.status(400).json({ message: "Data Not Found" });
 };
 
-export const GetTopRestaurants = async (req: Request, res: Response, next: NextFunction) => {};
+export const GetTopRestaurants = async (req: Request, res: Response, next: NextFunction) => {
+  const pincode = req.params.pincode;
+
+  const result = await Vendor.find({ pincode: pincode, serviceAvailable: true }).sort("rating descending").limit(10);
+
+  if (result.length > 0) {
+    return res.status(200).json(result);
+  }
+
+  return res.status(400).json({ message: "Data Not Found" });
+};
 
 export const GetFoodsIn30Min = async (req: Request, res: Response, next: NextFunction) => {};
 
