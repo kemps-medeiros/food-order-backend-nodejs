a2f5c5f3d5c8d901f4571740699ffa47b6306287
Kemps Medeiros <kempsmm@gmail.com>
2022-09-16 11:20:54 -0300

    feat: creating routes for customer

--- routes/CustomerRoute.ts
+++ routes/CustomerRoute.ts
@@ -2,4 +2,21 @@ import express, { NextFunction, Request, Response } from "express";
 
 const router = express.Router();
 
+/**------------SignUp / Create Customer ------*/
+router.post("/signup");
+
+/**------------Login-------------- */
+router.post("/login");
+/**------------Verify Customer Account ---------- */
+router.patch("/verify");
+/**------------OTP / Requesting OTP------------------ */
+router.get("/otp");
+/**------------Profile------------------ */
+
+router.get("/profile");
+router.patch("/profile");
+//Cart
+//Order
+//Payment
+
 export { router as CustomerRoute };
