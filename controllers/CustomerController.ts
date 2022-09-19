c4c1abaefa3f967540b50b0909afe0f455a771a9
Kemps Medeiros <kempsmm@gmail.com>
2022-09-19 09:49:59 -0300

    feat: implementing customer sign up

--- controllers/CustomerController.ts
+++ controllers/CustomerController.ts
@@ -1,6 +1,48 @@
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 import { NextFunction, Request, Response } from "express";
+import { CreateCustomerInputs } from "../dto/Customer.dto";
+import { Customer } from "../models/Customer";
+import { GeneratePasswordEncrypted, GenerateSalt } from "../utility";
 
-export const CustomerSignup = async (req: Request, res: Response, next: NextFunction) => {};
+export const CustomerSignup = async (req: Request, res: Response, next: NextFunction) => {
+  const customerInputs = plainToClass(CreateCustomerInputs, req.body);
+
+  const inputErrors = await validate(customerInputs, { validationError: { target: true } });
+
+  if (inputErrors.length > 0) {
+    return res.status(400).json(inputErrors);
+  }
+
+  const { email, phone, password } = customerInputs;
+
+  const salt = await GenerateSalt();
+  const userPassword = await GeneratePasswordEncrypted(password, salt);
+
+  const otp = 5698596;
+  const otp_expiry = new Date();
+
+  const result = await Customer.create({
+    email: email,
+    password: password,
+    salt: salt,
+    phone: phone,
+    otp: otp,
+    otp_expiry: otp_expiry,
+    firstName: "",
+    lastName: "",
+    address: "",
+    verified: false,
+    lat: 0,
+    lng: 0,
+  });
+
+  if (result) {
+    //send OTP to customer
+    //generate the signature
+    //send the result to client
+  }
+};
 
 export const CustomerLogin = async (req: Request, res: Response, next: NextFunction) => {};
 
